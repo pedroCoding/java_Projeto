fed60ee68fa547479da4f63375c341d36677d0a7
pedroCoding <falecompedrodev@gmail.com>
2022-12-10 20:14:33 -0300

    .

--- java_Project/src/java_Project/conta.java
+++ java_Project/src/java_Project/conta.java
@@ -3,6 +3,14 @@ package java_Project;
 public class conta {
 	
 	public static void main(String [] Args ) {
-		System.out.println("teste123");
+		
+		int Conta;
+		int Agencia;
+		String Titular;
+		int Valor;
+		
+		void Deposito(int Conta, int Agencia) {
+			System.out.println("Deposito : ");
+		}
 	}
 }
